0a5d8fba226424b8d7cdb86df247e2ba10d77de2
Julio Bazalar <jcbazalar@gmail.com>
2023-06-30 21:36:20 -0500

    Improve file's order and remove babel

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "express";
 import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 
 //Routes
 app.use("/auth", authRoute);
+app.use("/user", userRoute);
 
 export default app;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,16 +2,12 @@ import { Router } from "express";
 import {
   loginController,
   registerController,
-  updateProfileController,
 } from "../controller/auth.controller.js";
-import { tokenVerification } from "../utils/middelwares.js";
 
-const router = Router();
+const authRouter = Router();
 
-router.post("/register", registerController);
+authRouter.post("/register", registerController);
 
-router.post("/login", loginController);
+authRouter.post("/login", loginController);
 
-router.post("/update", tokenVerification, updateProfileController);
-
-export default router;
+export default authRouter;
